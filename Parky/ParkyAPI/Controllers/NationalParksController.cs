95d350ff30846bb91b26578c97f1eef6db6cccb9
henokg <henokg@insa.com>
2023-03-25 14:39:35 -0600

    Add a post method on national park controller api

--- Parky/ParkyAPI/Controllers/NationalParksController.cs
+++ Parky/ParkyAPI/Controllers/NationalParksController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
 using ParkyAPI.Models.Dtos;
 using ParkyAPI.Repository.IRepository;
 using System;
@@ -32,7 +33,8 @@ namespace ParkyAPI.Controllers
             }
             return Ok(objDto);
         }
-        [HttpGet("{nationalParkId:int}")]
+        //[HttpGet("{nationalParkId:int}")]
+        [HttpGet("{nationalParkId:int}",Name = "GetNationalPark")]
         public IActionResult GetNationalPark(int nationalParkId)
         {
             var obj = _npRepo.GetNationalPark(nationalParkId);
@@ -42,6 +44,31 @@ namespace ParkyAPI.Controllers
             }
             var objDto = _mapper.Map<NationalParkDto>(obj);
             return Ok(objDto);
+        }
+        [HttpPost]
+        public IActionResult CreateNationalPark([FromBody] NationalParkDto nationalParkDto)
+        {
+            if (nationalParkDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+            if (_npRepo.NationalParkExists(nationalParkDto.Name))
+            {
+                ModelState.AddModelError("", "National Park Exists!");
+                return StatusCode(404, ModelState);
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var nationalParkObj = _mapper.Map<NationalPark>(nationalParkDto);
+            if (!_npRepo.CreateNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when saving the record {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+            //return Ok();
+            return CreatedAtRoute("GetNationalPark", new { nationalParkId = nationalParkObj.Id }, nationalParkObj);
         }
     }
 }
