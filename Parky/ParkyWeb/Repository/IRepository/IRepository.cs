9e340c6d704338b1c1d5894cb30bc6f445942d09
henokg <heni.gebrehiwot@gmail.com>
2023-04-12 23:58:10 +0300

    Section 8 - Repository Other Methods

--- Parky/ParkyWeb/Repository/IRepository/IRepository.cs
+++ Parky/ParkyWeb/Repository/IRepository/IRepository.cs
@@ -5,7 +5,12 @@ using System.Threading.Tasks;
 
 namespace ParkyWeb.Repository.IRepository
 {
-    interface IRepository
+    interface IRepository<T> where T : class
     {
+        Task<T> GetAsync(string url, int Id);
+        Task<IEnumerable<T>> GetAllAsync(string url);
+        Task<bool> CreateAsync(string url, T objToCreate);
+        Task<bool> UpdateAsync(string url, T objToUpdate);
+        Task<bool> DeleteAsync(string url, int Id);
     }
 }
