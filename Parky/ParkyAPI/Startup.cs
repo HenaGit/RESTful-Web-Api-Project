e6e0d65db21141663338b5fe747cb8ee70d34157
henokg <heni.gebrehiwot@gmail.com>
2023-04-12 21:37:41 +0300

    Section 7 -Api Versioning Part 1

--- Parky/ParkyAPI/Startup.cs
+++ Parky/ParkyAPI/Startup.cs
@@ -39,13 +39,19 @@ namespace ParkyAPI
             services.AddScoped<INationalParkRepository, NationalParkRepository>();
             services.AddScoped<ITrailRepository, TrailRepository>();
             services.AddAutoMapper(typeof(ParkyMappings));
+            services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+            });
             services.AddSwaggerGen(options => {
-                options.SwaggerDoc("ParkyOpenAPISpecNP",
+                options.SwaggerDoc("ParkyOpenAPISpec",
                     new Microsoft.OpenApi.Models.OpenApiInfo()
                     {
-                        Title = "Parky API (National Park)",
+                        Title = "Parky API",
                         Version = "1",
-                        Description = "Hena Parky API NP",
+                        Description = "Hena Parky API",
                         Contact = new Microsoft.OpenApi.Models.OpenApiContact()
                         {
                             Email = "heni.gebrehiwot@gmail.com",
@@ -58,25 +64,25 @@ namespace ParkyAPI
                             Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
                         }
                     });
-                options.SwaggerDoc("ParkyOpenAPISpecTrails",
-                  new Microsoft.OpenApi.Models.OpenApiInfo()
-                  {
-                      Title = "Parky API Trails",
-                      Version = "1",
-                      Description = "Hena Parky API Trails",
-                      Contact = new Microsoft.OpenApi.Models.OpenApiContact()
-                      {
-                          Email = "heni.gebrehiwot@gmail.com",
-                          Name = "Henok Gebrehiwot",
-                          Url = new Uri("https://wwww.codewithhena.com")
-                      },
-                      License = new Microsoft.OpenApi.Models.OpenApiLicense()
-                      {
-                          Name = "MIT License",
-                          Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
-                      }
+                //options.SwaggerDoc("ParkyOpenAPISpecTrails",
+                //  new Microsoft.OpenApi.Models.OpenApiInfo()
+                //  {
+                //      Title = "Parky API Trails",
+                //      Version = "1",
+                //      Description = "Hena Parky API Trails",
+                //      Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                //      {
+                //          Email = "heni.gebrehiwot@gmail.com",
+                //          Name = "Henok Gebrehiwot",
+                //          Url = new Uri("https://wwww.codewithhena.com")
+                //      },
+                //      License = new Microsoft.OpenApi.Models.OpenApiLicense()
+                //      {
+                //          Name = "MIT License",
+                //          Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
+                //      }
 
-                  });
+                //  });
                 var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var cmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
                 options.IncludeXmlComments(cmlCommentsFullPath);
@@ -95,8 +101,8 @@ namespace ParkyAPI
             app.UseHttpsRedirection();
             app.UseSwagger();
             app.UseSwaggerUI(options => {
-                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecNP/swagger.json", "Parky API NP");
-                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecTrails/swagger.json", "Parky API Trails");
+                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpec/swagger.json", "Parky API");
+                //options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecTrails/swagger.json", "Parky API Trails");
                 options.RoutePrefix = "";
             });
             app.UseRouting();
