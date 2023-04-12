9ba5d4f88df911d8dd2d2c3824cff10b7145c034
henokg <heni.gebrehiwot@gmail.com>
2023-04-12 21:20:56 +0300

    Section 7 - Multiple Open API Documentation

--- Parky/ParkyAPI/Startup.cs
+++ Parky/ParkyAPI/Startup.cs
@@ -40,12 +40,12 @@ namespace ParkyAPI
             services.AddScoped<ITrailRepository, TrailRepository>();
             services.AddAutoMapper(typeof(ParkyMappings));
             services.AddSwaggerGen(options => {
-                options.SwaggerDoc("ParkyOpenAPISpec",
+                options.SwaggerDoc("ParkyOpenAPISpecNP",
                     new Microsoft.OpenApi.Models.OpenApiInfo()
                     {
-                        Title = "Parky API",
+                        Title = "Parky API (National Park)",
                         Version = "1",
-                        Description = "Hena Parky API",
+                        Description = "Hena Parky API NP",
                         Contact = new Microsoft.OpenApi.Models.OpenApiContact()
                         {
                             Email = "heni.gebrehiwot@gmail.com",
@@ -58,6 +58,25 @@ namespace ParkyAPI
                             Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
                         }
                     });
+                options.SwaggerDoc("ParkyOpenAPISpecTrails",
+                  new Microsoft.OpenApi.Models.OpenApiInfo()
+                  {
+                      Title = "Parky API Trails",
+                      Version = "1",
+                      Description = "Hena Parky API Trails",
+                      Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                      {
+                          Email = "heni.gebrehiwot@gmail.com",
+                          Name = "Henok Gebrehiwot",
+                          Url = new Uri("https://wwww.codewithhena.com")
+                      },
+                      License = new Microsoft.OpenApi.Models.OpenApiLicense()
+                      {
+                          Name = "MIT License",
+                          Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
+                      }
+
+                  });
                 var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var cmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
                 options.IncludeXmlComments(cmlCommentsFullPath);
@@ -76,7 +95,8 @@ namespace ParkyAPI
             app.UseHttpsRedirection();
             app.UseSwagger();
             app.UseSwaggerUI(options => {
-                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpec/swagger.json", "Parky API");
+                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecNP/swagger.json", "Parky API NP");
+                options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecTrails/swagger.json", "Parky API Trails");
                 options.RoutePrefix = "";
             });
             app.UseRouting();
