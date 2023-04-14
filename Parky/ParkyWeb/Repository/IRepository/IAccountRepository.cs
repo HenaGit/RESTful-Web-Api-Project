8f3ce647b6a2e31376d48b59aae3388b74998215
henokg <heni.gebrehiwot@gmail.com>
2023-04-14 21:15:22 +0300

    Section 13 - Implement Login and Register in Account Repository

--- Parky/ParkyWeb/Repository/IRepository/IAccountRepository.cs
+++ Parky/ParkyWeb/Repository/IRepository/IAccountRepository.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using ParkyWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ParkyWeb.Repository.IRepository
 {
-    interface IAccountRepository
+    public interface IAccountRepository : IRepository<User>
     {
+        Task<User> LoginAsync(string url, User objToCreate);
+        Task<bool> RegisterAsync(string url, User objToCreate);
     }
 }
